3bfe8d5c1a873338079adeaa71cf56da23d3cb8d
xstahol <staffan.holmgren@tre.se>
2021-12-03 09:37:50 +0100

    d3 part one

--- lib/d3.ex
+++ lib/d3.ex
@@ -12,13 +12,13 @@ defmodule D3 do
                    |> Enum.zip()
                    |> Enum.map(&Tuple.to_list/1)
                    |> IO.inspect()
-                   |> Enum.map(&find_most_common(&1, entries_count))
+                   |> Enum.map(&convert_bit_to_gamma_bit(&1, entries_count))
 
     parsed_epsilon = input
                      |> Enum.zip()
                      |> Enum.map(&Tuple.to_list/1)
                      |> IO.inspect()
-                     |> Enum.map(&find_least_common(&1, entries_count))
+                     |> Enum.map(&convert_bit_to_epsilon_bit(&1, entries_count))
 
     gamma = parsed_gamma
             |> Enum.join()
@@ -33,7 +33,7 @@ defmodule D3 do
     epsilon * gamma
   end
 
-  def find_most_common(list, entries_count) do
+  def convert_bit_to_gamma_bit(list, entries_count) do
     ones_count = Enum.count(list, fn x -> x == "1" end)
     if(ones_count > entries_count / 2) do
       "1"
@@ -42,7 +42,7 @@ defmodule D3 do
     end
   end
 
-  def find_least_common(list, entries_count) do
+  def convert_bit_to_epsilon_bit(list, entries_count) do
     zeros_count = Enum.count(list, fn x -> x == "0" end)
     if(zeros_count > entries_count / 2) do
       "1"
@@ -51,33 +51,4 @@ defmodule D3 do
     end
   end
 
-
-
-  def parse_binary(binary) do
-    matrix = binary
-             |> String.graphemes()
-             |> IO.inspect()
-             |> Enum.flat_map(
-                  fn binary ->
-                    String.graphemes(binary)
-                    |> Enum.map(&String.to_integer/1)
-                    #    |> Enum.map(&map_bit/1)
-                  end
-                )
-    #Enum.reduce(matrix, )
-  end
-
-  def map_bit(bit) do
-    if bit == 0 do
-      {1, 0}
-    else
-      {0, 1}
-    end
-  end
-
-  def calc_gamma(binary_list) do
-
-
-
-  end
 end
