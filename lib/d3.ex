62b9920762082fa2101da0de751c66d5bbe351e0
xstahol <staffan.holmgren@tre.se>
2021-12-03 09:39:44 +0100

    d3 part one

--- lib/d3.ex
+++ lib/d3.ex
@@ -3,15 +3,12 @@ defmodule D3 do
   def one() do
     input = Read_File_Utils.read_file("d3.txt")
             |> Enum.map(&String.graphemes/1)
-            |> IO.inspect()
 
     entries_count = Enum.count(input)
-                    |> IO.inspect()
 
     gamma = input
             |> Enum.zip()
             |> Enum.map(&Tuple.to_list/1)
-            |> IO.inspect()
             |> Enum.map(&convert_bit_to_gamma_bit(&1, entries_count))
             |> Enum.join()
             |> String.to_integer(2)
@@ -19,7 +16,6 @@ defmodule D3 do
     epsilon = input
               |> Enum.zip()
               |> Enum.map(&Tuple.to_list/1)
-              |> IO.inspect()
               |> Enum.map(&convert_bit_to_epsilon_bit(&1, entries_count))
               |> Enum.join()
               |> String.to_integer(2)
