d99391f8f3ecbabe5434c177bc9873040fc44afe
xstahol <staffan.holmgren@tre.se>
2021-12-03 14:43:52 +0100

    d3 part two...improved

--- lib/d3.ex
+++ lib/d3.ex
@@ -9,30 +9,28 @@ defmodule D3 do
     gamma = input
             |> Enum.zip()
             |> Enum.map(&Tuple.to_list/1)
-            |> Enum.map(&convert_bit_to_gamma_bit(&1, entries_count))
+            |> Enum.map(&find_most_common_bit(&1))
             |> Enum.join()
             |> String.to_integer(2)
 
     epsilon = input
               |> Enum.zip()
               |> Enum.map(&Tuple.to_list/1)
-              |> Enum.map(&convert_bit_to_epsilon_bit(&1, entries_count))
+              |> Enum.map(&find_least_common_bit(&1))
               |> Enum.join()
               |> String.to_integer(2)
 
     epsilon * gamma
   end
 
-  def convert_bit_to_gamma_bit(list, entries_count) do
+  def find_most_common_bit(list) do
     ones_count = Enum.count(list, fn x -> x == "1" end)
-                 |> IO.inspect(label: "ones count")
     zero_count = Enum.count(list, fn x -> x == "0" end)
-                 |> IO.inspect(label: "zero count")
 
     if(ones_count == zero_count) do
       "1"
     else
-      if(ones_count > entries_count / 2) do
+      if(ones_count > zero_count) do
         "1"
       else
         "0"
@@ -41,7 +39,7 @@ defmodule D3 do
     end
   end
 
-  def convert_bit_to_epsilon_bit(list, entries_count) do
+  def find_least_common_bit(list) do
     zeros_count = Enum.count(list, fn x -> x == "0" end)
     ones_count = Enum.count(list, fn x -> x == "1" end)
 
@@ -62,16 +60,12 @@ defmodule D3 do
 
     o2 = calc_o2(input)
          |> String.to_integer(2)
-         |> IO.inspect(label: "co2")
     co2 = calc_co2(input)
           |> String.to_integer(2)
-          |> IO.inspect(label: "co2")
     co2 * o2
   end
 
   def calc_o2(binaries, bit \\ 0) do
-    IO.inspect(binaries, label: "start")
-    IO.inspect("bit: #{bit}")
     if(Enum.count(binaries) == 1) do
       Enum.at(binaries, 0)
       |> Enum.join()
@@ -80,9 +74,8 @@ defmodule D3 do
       structured = binaries
                    |> Enum.zip()
                    |> Enum.map(&Tuple.to_list/1)
-                   |> IO.inspect(label: "structured")
 
-      bit_to_save = convert_bit_to_gamma_bit(Enum.at(structured, bit), entries)
+      bit_to_save = find_most_common_bit(Enum.at(structured, bit))
                     |> IO.inspect(label: "to save")
 
       calc_o2(
@@ -94,19 +87,15 @@ defmodule D3 do
   end
 
   def calc_co2(binaries, bit \\ 0) do
-    IO.inspect(binaries, label: "start")
-    IO.inspect("bit: #{bit}")
     if(Enum.count(binaries) == 1) do
       Enum.at(binaries, 0)
-      |> IO.inspect(label: "ttt")
       |> Enum.join()
     else
-      entries = Enum.count(binaries)
       structured = binaries
                    |> Enum.zip()
                    |> Enum.map(&Tuple.to_list/1)
 
-      bit_to_save = convert_bit_to_epsilon_bit(Enum.at(structured, bit), entries)
+      bit_to_save = find_least_common_bit(Enum.at(structured, bit))
                     |> IO.inspect(label: "to save")
 
       calc_co2(
